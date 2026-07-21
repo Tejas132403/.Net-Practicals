e55203e38fae4b13fedc73357c5c09eb1b0d6140
Tejas Andodariya <tejas.andodariya132403@marwadiuniversity.ac.in>
2026-07-21 19:36:07 +0530

    Add StudentAdmissionManagement to Practical-1

---
+++ Practical-1/StudentAdmissionManagement/StudentAdmissionManagement/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace StudentAdmissionManagement
+{
+    class Student
+    {
+        private int id;
+        private string name;
+        private string course;
+        private double fees;
+
+        public Student(int id, string name, string course, double fees)
+        {
+            this.id = id;
+            this.name = name;
+            this.course = course;
+            this.fees = fees;
+        }
+
+        public void DisplayDetails()
+        {
+            Console.WriteLine("------ Student Admission Details ------");
+            Console.WriteLine("Student ID : " + id);
+            Console.WriteLine("Student Name : " + name);
+            Console.WriteLine("Course : " + course);
+            Console.WriteLine("Fees : " + fees);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student(122, "Tejas Andodariya", "Computer Engineering", 85000);
+            Student s2 = new Student(123, "King of Universe", " Engineering", 75000);
+
+            s1.DisplayDetails();
+            s2.DisplayDetails();
+
+            Console.ReadKey();
+        }
+    }
+}
